12683ec5fd24dfce601ae644ff6b098a9a67fc3a
jonathanarieln <jonathan.ariel.nunez@gmail.com>
2016-10-08 00:32:54 -0600

    Add files via upload

---
+++ tbl_bitacora.sql
@@ -0,0 +1,59 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 08-10-2016 a las 08:32:35
+-- Versión del servidor: 10.1.16-MariaDB
+-- Versión de PHP: 7.0.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `procacaho_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbl_bitacora`
+--
+
+CREATE TABLE `tbl_bitacora` (
+  `accion_realizada` varchar(30) NOT NULL,
+  `codigo_usuario` int(10) NOT NULL,
+  `fecha` datetime NOT NULL,
+  `comentario` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `tbl_bitacora`
+--
+ALTER TABLE `tbl_bitacora`
+  ADD PRIMARY KEY (`accion_realizada`),
+  ADD KEY `codigo_usuario` (`codigo_usuario`);
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `tbl_bitacora`
+--
+ALTER TABLE `tbl_bitacora`
+  ADD CONSTRAINT `tbl_bitacora_ibfk_1` FOREIGN KEY (`codigo_usuario`) REFERENCES `tbl_usuarios` (`codigo_usuario`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
